be91b0748d982400ed0545604885f91a8ba39225
Lucas Morey <Lucas@MacBook-Pro.local>
2017-08-07 16:20:26 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cards[0]);
+console.log("user flipped " + cards[0]);
+cardsInPlay.push(cards[2]);
+console.log("user flipped " + cards[2]);
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+if (cardsInPlay.length === 2) 
+	if  (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You've found a match!");
+}
+
+else  { 
+	alert("Sorry, try again.");
+}
+
